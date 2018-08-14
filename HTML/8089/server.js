65220b05fd50ac44cb46bc9cb2ab15d414bcf522
杜召磊 <474256233@qq.com>
2018-08-14 09:27:31 +0800

    new

---
+++ HTML/8089/server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var domRoot = 'D:/work/learnAndpractice/HTML/8089'
+
+var server = http.createServer(function (req,res) {
+  console.log('连接成功')
+  var url = req.url;
+
+  if(url== '/'){
+    url ='index.html';
+  }
+  var file = domRoot + url
+  console.log(url)
+  fs.readFile(file,function (err,data) {
+    if(err){
+      res.writeHeader(404,{
+        "content-type":'text/html;charset="utf-8"'
+      })
+      res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
+      res.end()
+    }else {
+      res.writeHeader(200,{
+        "content-type":'text/html;charset="utf-8"'
+      })
+      res.write(data);
+      res.end()
+    }
+  })
+}).listen(8089)
+
+console.log('服务器开启')
